8c859c0b089ec9c04c0ad92ce00ffe57da92fd37
jeungHunLee <ksjs1111@gmail.com>
2024-05-10 10:15:03 +0900

    initial commit

---
+++ Baobab/BaobabApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaobabApp.swift
+//  Baobab
+//
+//  Created by 이정훈 on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BaobabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Baobab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Baobab
+//
+//  Created by 이정훈 on 5/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
