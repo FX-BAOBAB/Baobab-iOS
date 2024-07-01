97e936569427d171c171c695ab8a9572f3c026ff
jeungHunLee <ksjs1111@gmail.com>
2024-07-01 13:47:38 +0900

    IB-26 FEAT: 로그인 UI 구현

---
+++ Baobab/Presentation/LogIn/View/LoginInputBox.swift
@@ -0,0 +1,18 @@
+//
+//  LoginInputBox.swift
+//  Baobab
+//
+//  Created by 이정훈 on 7/1/24.
+//
+
+import SwiftUI
+
+struct LoginInputBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginInputBox()
+}
