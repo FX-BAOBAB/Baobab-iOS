d6f3278e0f9940ec444ccc13fd784349d7b3256f
jeungHunLee <ksjs1111@gmail.com>
2024-07-01 14:54:12 +0900

    IB-29 FEAT: 주소가 정확하게 입력되지 않은 경우 alert 출력기능 추가

--- Baobab/Presentation/SignUp/View/AddressRegistrationForm.swift
+++ Baobab/Presentation/SignUp/View/AddressRegistrationForm.swift
@@ -11,13 +11,14 @@ struct AddressRegistrationForm: View {
     @EnvironmentObject private var viewModel: SignUpViewModel
     @State private var isShowingPostCodeSearchForm: Bool = false
     @State private var isShowingCompletionView: Bool = false
+    @State private var isShowingEmptyAddressAlert: Bool = false
     @Binding private(set) var isShowingSignUpForm: Bool
     @Binding private(set) var isShowingAddressRegistrationForm: Bool
     
     var body: some View {
         ZStack {
             VStack(alignment: .leading) {
-                SearchedAddressView(isShowingPostCodeSearch: $isShowingPostCodeSearchForm)
+                SearchedAddressView(isShowingPostCodeSearchForm: $isShowingPostCodeSearchForm)
                     .environmentObject(viewModel)
                     .padding(.bottom)
                 
@@ -28,22 +29,23 @@ struct AddressRegistrationForm: View {
             }
             .padding()
             
-            VStack {
-                Button(action: {
+            Button(action: {
+                if viewModel.detailedAddressInput.isEmpty {
+                    isShowingEmptyAddressAlert.toggle()
+                } else {
                     //TODO: 회원 가입 결과에 따라 회원가입 완료 View로 이동
-                }, label: {
-                    Text("다음")
-                        .bold()
-                        .padding(8)
-                        .foregroundColor(.white)
-                        .frame(maxWidth: .infinity)
-                })
-                .buttonBorderShape(.roundedRectangle)
-                .cornerRadius(10)
-                .buttonStyle(.borderedProminent)
-                .padding([.leading, .trailing, .bottom])
-                .background(.white)
-            }
+                }
+            }, label: {
+                Text("다음")
+                    .bold()
+                    .padding(8)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+            })
+            .cornerRadius(10)
+            .buttonStyle(.borderedProminent)
+            .padding([.leading, .trailing, .bottom])
+            .background(.white)
             .frame(maxHeight: .infinity, alignment: .bottom)
             .ignoresSafeArea(.keyboard)    //TextField가 활성화 되어도 위치 고정
         }
@@ -57,6 +59,11 @@ struct AddressRegistrationForm: View {
             SignUpCompletionView(isShowingCompletionView: $isShowingCompletionView,
                                  isShowingAddressRegistrationForm: $isShowingAddressRegistrationForm,
                                  isShowingSignUpForm: $isShowingSignUpForm)
+        }
+        .alert(isPresented: $isShowingEmptyAddressAlert) {
+            Alert(title: Text("알림"),
+                  message: Text("방문지 정보를 정확하게 입력해 주세요"),
+                  dismissButton: .default(Text("확인")))
         }
     }
 }
