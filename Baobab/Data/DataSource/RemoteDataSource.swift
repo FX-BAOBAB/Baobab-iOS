91bcf384574ecbcfc137bbe320ba2919ef5a7e28
jeungHunLee <ksjs1111@gmail.com>
2024-07-10 08:39:35 +0900

    IB-34 FEAT: 회원가입 Domain, Data layer 구현

--- Baobab/Data/DataSource/RemoteDataSource.swift
+++ Baobab/Data/DataSource/RemoteDataSource.swift
@@ -17,11 +17,17 @@ protocol RemoteDataSource {
 }
 
 final class RemoteDataSourceImpl: RemoteDataSource {
+    private let session: Session
+    
+    init(session: Session = Session.default) {
+        self.session = session
+    }
+    
     //MARK: - GET Request
     func sendGetRequest<DTO>(to url: String, 
                              resultType: DTO.Type) -> AnyPublisher<DTO, any Error> where DTO: Decodable {
-        return AF.request(url)
-                    .publishDecodable()
+        return session.request(url)
+                    .publishDecodable(type: resultType)
                     .value()
                     .mapError {
                         $0 as Error
@@ -33,11 +39,11 @@ final class RemoteDataSourceImpl: RemoteDataSource {
     func sendPostRequest<DTO>(to url: String, 
                               with params: Parameters,
                               resultType: DTO.Type) -> AnyPublisher<DTO, any Error> where DTO: Decodable {
-        return AF.request(url,
+        return session.request(url,
                           method: .post,
                           parameters: params,
                           encoding: JSONEncoding.default)
-                    .publishDecodable()
+                    .publishDecodable(type: resultType)
                     .value()
                     .mapError {
                         $0 as Error
