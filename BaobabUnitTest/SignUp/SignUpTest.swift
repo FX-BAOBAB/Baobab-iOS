67ef6d4854b393a3561064195cfd0d147baa6ec0
jeungHunLee <ksjs1111@gmail.com>
2024-07-10 13:08:21 +0900

    IB-37 TEST: 회원가입 api 네트워크 테스트

--- BaobabUnitTest/SignUp/SignUpTest.swift
+++ BaobabUnitTest/SignUp/SignUpTest.swift
@@ -35,13 +35,7 @@ final class SignUpTest: XCTestCase {
         let session = Session(configuration: configuration)
         dataSource = RemoteDataSourceImpl(session: session)
         let apiEndPoint = Bundle.main.apiUrl + "/open-api/users"
-        let params: [String: Any] = ["email": "baobab@baobab.com",
-                                    "password": "Password1!",
-                                    "name": "홍길동",
-                                    "address": "주소",
-                                    "detailAddress": "상세주소",
-                                    "basicAddress": true,
-                                    "post": 123456]
+        let params: [String: Any] = [String: Any]()
         let expectation = XCTestExpectation(description: "Performs a request")
         
         //When
@@ -68,6 +62,7 @@ final class SignUpTest: XCTestCase {
     }
     
     func test_signUpRequest_responseCode_1154() {
+        //Given
         let configuration = URLSessionConfiguration.default
         configuration.protocolClasses = [SignUpFailMockURLProtocol.self]
         
@@ -75,22 +70,17 @@ final class SignUpTest: XCTestCase {
         dataSource = RemoteDataSourceImpl(session: session)
         
         let apiEndPoint = Bundle.main.apiUrl + "/open-api/users"
-        let params: [String: Any] = ["email": "baobab@baobab.com",
-                                    "password": "Password1!",
-                                    "name": "홍길동",
-                                    "address": "주소",
-                                    "detailAddress": "상세주소",
-                                    "basicAddress": true,
-                                    "post": 123456]
+        let params: [String: Any] = [String: Any]()
         let expectation = XCTestExpectation(description: "Performs a request")
         
+        //When
         dataSource.sendPostRequest(to: apiEndPoint, with: params, resultType: SignUpResponseDTO.self)
             .sink(receiveCompletion: { completion in
                 switch completion {
                 case .finished:
                     print("Sign-up request has been completed")
                 case .failure(let error):
-                    print("test_signUpRequest_responseCode_200() error : " , error)
+                    print("test_signUpRequest_responseCode_1154() error : " , error)
                 }
             }, receiveValue: {
                 //Then
@@ -98,6 +88,52 @@ final class SignUpTest: XCTestCase {
                 XCTAssertEqual($0.result.resultDescription, "실패")
                 XCTAssertEqual($0.result.resultMessage, "이미 존재하는 아이디입니다.")
                 XCTAssertNil($0.body?.message)
+                
+                expectation.fulfill()
+            })
+            .store(in: &cancellables)
+        
+        wait(for: [expectation], timeout: 5)
+    }
+    
+    func test_signUp() {
+        //Given
+        dataSource = RemoteDataSourceImpl()
+        
+        let apiEndPoint = Bundle.main.apiUrl + "/open-api/users"
+        let params: [String: Any] = [
+            "result": [
+                "resultCode": 0,
+                "resultMessage": "string",
+                "resultDescription": "string"
+            ],
+            "body": [
+                "email": "test@baobab.com",
+                "password": "Password1!",
+                "name": "홍길동",
+                "address": "string",
+                "detailAddress": "string",
+                "basicAddress": true,
+                "post": 123456
+            ]
+        ]
+        let expectation = XCTestExpectation(description: "Performs a request")
+        
+        //When
+        dataSource.sendPostRequest(to: apiEndPoint, with: params, resultType: SignUpResponseDTO.self)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    print("Sign-up request has been completed")
+                case .failure(let error):
+                    print("test_signUp() error : " , error)
+                }
+            }, receiveValue: {
+                //Then
+                XCTAssertEqual($0.result.resultCode, 200)
+                XCTAssertEqual($0.result.resultDescription, "성공")
+                XCTAssertEqual($0.result.resultMessage, "성공")
+                XCTAssertEqual($0.body?.message, "회원 가입이 완료되었습니다.")
                 
                 expectation.fulfill()
             })
