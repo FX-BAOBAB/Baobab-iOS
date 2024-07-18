5ce4cffc66852adbd6e4706d3af3eedfbcf5e098
jeungHunLee <ksjs1111@gmail.com>
2024-07-18 15:57:43 +0900

    IB-60 FIX: 입고 관련 Domain, Data Layer 버그 수정

--- Baobab/Domain/UseCases/ReceivingUseCase.swift
+++ Baobab/Domain/UseCases/ReceivingUseCase.swift
@@ -10,9 +10,18 @@ import Combine
 
 //MARK: - ReceivingUseCase protocol
 protocol ReceivingUseCase {
+    /// 기본주소로 등록되어 있는 주소를 요청하는 함수
     func fetchDefaultAddress() -> AnyPublisher<Address, any Error>
+    
+    /// 등록된 모든 주소를 요청하는 함수
     func fetchAddresses() -> AnyPublisher<[Address], any Error>
+    
+    /// 인자로 전달된 주소의 Geo Code를 요청하는 함수
+    /// - Parameter address: 주소 문자열
     func fetchGeoCode(of address: String) -> AnyPublisher<MKCoordinateRegion, any Error>
+    
+    /// - Parameter params: 방문 신청 날짜, 방문지 주소, 결함 인정 시간에 대한 딕셔너리
+    /// - Parameter items: 입고 물품 객체 배열
     func execute(params: [String: Any], items: [StoreItem]) -> AnyPublisher<Bool, any Error>
 }
 
@@ -49,7 +58,7 @@ extension ReceivingUseCaseImpl: ReceivingUseCase {
 }
 
 extension ReceivingUseCaseImpl{
-    struct IdentifiedReponse {
+    private struct IdentifiedReponse {
         let index: Int
         let response: [ImageUploadResponse]
     }
@@ -61,19 +70,25 @@ extension ReceivingUseCaseImpl{
             [
                 "name": item.itemName,
                 "modelName": item.itemName,
-                "category": item.itemCategory ?? "",
+                "category": "SMALL_FURNITURE",    //TODO: 카테고리 동적으로 변경(영문명)
                 "quantity": item.itemQuantity,
                 "imageIdList": [Int]()
-            ]
+            ] as [String: Any]
         }
-        params["goodsRequests"] = itemRequests
+
+        if var body = params["body"] as? [String: Any] {
+            body["goodsRequests"] = itemRequests
+            
+            params["body"] = body
+        }
         
         for (i, item) in items.enumerated() {
             //물품 이미지 업로드
+            //어차피 itemImages는 모두 값이 존재해야하기 때문에 dataSource의 [Data]로 타입 캐스팅 가능함.
             let itemImagePublisher = uploadImageUseCase.execute(params: [
-                    "files": item.itemImages.compactMap { $0 },    //Optional에서 값 추출
+                    "files": item.itemImages,
                     "kind": "BASIC",
-                    "caption": Array(repeating: "", count: item.itemImages.count)
+                    "captions": Array(repeating: "캡션", count: item.itemImages.count)
                 ])
                 .map {
                     IdentifiedReponse(index: i, response: $0)
@@ -84,7 +99,7 @@ extension ReceivingUseCaseImpl{
             let defectImagePublisher = uploadImageUseCase.execute(params: [
                     "files": item.defects.map { $0.image },
                     "kind": "FAULT",
-                    "caption": item.defects.map { $0.description }
+                    "captions": item.defects.map { $0.description }
                 ])
                 .map {
                     IdentifiedReponse(index: i, response: $0)
@@ -99,18 +114,26 @@ extension ReceivingUseCaseImpl{
         //MargeMany는 여러개의 Publisher를 하나의 Publisher로 결합
         //각 Publisher의 결과 값을 하나의 배열에 담아서 방출
         return Publishers.MergeMany(publishers)
-            .collect()
+            .collect()    //publisher들이 방출하는 값을 배열에 모아서 한번에 처리
             .flatMap { identifiedResponse -> AnyPublisher<Bool, any Error> in
                 identifiedResponse.forEach {
-                    if let goodsRequests = params["goodsRequests"] as? [[String: Any]] {
-                        if var imageIdList = goodsRequests[$0.index]["imageIdList"] as? [Int] {
-                            imageIdList.append(contentsOf: $0.response.map { response in
-                                return response.id
-                            })
+                    if var body = params["body"] as? [String: Any] {
+                        if var goodsRequests = body["goodsRequests"] as? [[String: Any]] {
+                            if var imageIdList = goodsRequests[$0.index]["imageIdList"] as? [Int] {
+                                imageIdList.append(contentsOf: $0.response.map { response in
+                                    return response.id
+                                })
+                                
+                                goodsRequests[$0.index]["imageIdList"] = imageIdList
+                            }
+                            
+                            body["goodsRequests"] = goodsRequests
                         }
+                        
+                        params["body"] = body
                     }
                 }
-            
+                
                 return self.repository.requestReceiving(params: params)
             }
             .eraseToAnyPublisher()
